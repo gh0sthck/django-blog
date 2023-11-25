42e1c25c67f92ba83b9f0a7547e0e77b2d814fd2
gh0st <maratka2202@gmail.com>
2023-11-25 18:00:15 +0300

    Created model and migrations for Post in models.py

--- blogsite/posts/models.py
+++ blogsite/posts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """Posts (articles) in blog."""
+
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Черновик"
+        PUBLISHED = "PB", "Опубликовано"
+
+    TITLE_LENGTH: int = 90
+
+    title = models.CharField(max_length=TITLE_LENGTH)
+    slug = models.SlugField(max_length=TITLE_LENGTH)
+    text = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ["-publish"]
+
+    def __str__(self) -> str:
+        return self.title

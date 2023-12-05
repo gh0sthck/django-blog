3aaf35dfb6b99b004bad886090d3fbc8442d8e34
gh0st <maratka2202@gmail.com>
2023-12-05 11:10:14 +0300

    Profile system dev start

--- blogsite/posts/urls.py
+++ blogsite/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import all_posts_page, post_detail_page
+from .views import all_posts_page, post_detail_page, user_page
 
 urlpatterns: list = [
     path("", all_posts_page, name="all_posts_page"),
-    path("<int:post_id>/", post_detail_page, name="post_detail_page")
+    path("<int:post_id>/", post_detail_page, name="post_detail_page"),
+    path("profile/<int:user_id>/", user_page, name="user_page")
 ]
--- blogsite/posts/views.py
+++ blogsite/posts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
@@ -21,3 +22,14 @@ def post_detail_page(request, post_id: int) -> render:
                       "post_detail.html",
                       {"post_title": current_post.title, "post_description": current_post.text,
                        "post_pubdate": current_post.publish, "post_author": current_post.author})
+
+
+def user_page(request, user_id: int) -> render:
+    try:
+        current_user: User = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        raise Http404("User not found.")
+    else:
+        return render(request,
+                      "user_page.html",
+                      {"username": current_user.username})
